3cee91fbb6329a03b9bdbb851330808758761af5
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-04-06 16:25:57 -0300

    feat: implementada classe ContaCorrente com métodos de saque, depósito, transferência e consulta de saldo

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,71 @@
+class ContaCorrente
+{
+  public int numeroIdentificacao;
+  public string titular;
+  public decimal saldo;
+  public decimal limiteDebito;
+
+  public void Sacar()
+  {
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite o valor que deseja sacar (R$): ");
+    decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+
+    if (valorSaque > saldo + limiteDebito)
+    {
+      Console.WriteLine("-------------------------------------");
+      Console.WriteLine("O valor do limite de débito foi ultrapassado!");
+    }
+    else
+    {
+      saldo -= valorSaque;
+      Console.WriteLine("-------------------------------------");
+      Console.WriteLine("O valor foi sacado com sucesso!");
+
+    }
+
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite ENTER para continuar...");
+    Console.ReadLine();
+  }
+
+  public void Depositar()
+  {
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite o valor que deseja depositar (R$): ");
+    decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
+
+    saldo += valorDeposito;
+
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine("O valor foi sacado com sucesso!");
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite ENTER para continuar...");
+    Console.ReadLine();
+  }
+
+  public void TransferirPara(ContaCorrente contaDestino)
+  {
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite o valor que deseja transferir (R$): ");
+    decimal valorTransferencia = Convert.ToDecimal(Console.ReadLine());
+
+    saldo -= valorTransferencia;
+    contaDestino.saldo += valorTransferencia;
+
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine($"O valor de R${valorTransferencia} foi tranferido com sucesso!");
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite ENTER para continuar...");
+    Console.ReadLine();
+  }
+
+  public void ObterSaldo()
+  {
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine("O valor do saldo da conta é de (R$): " + saldo);
+    Console.WriteLine("-------------------------------------");
+    Console.Write("Digite ENTER para continuar...");
+    Console.ReadLine();
+  }
+}
--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,60 +1,41 @@
 ﻿using System.Security.Cryptography;
 
-int numeroIdentificacao = RandomNumberGenerator.GetInt32(1, 101);
-string titular = "Natalia";
-decimal saldo = 1000;
-decimal limiteDebito = 1200;
-
-while (true)
+class Program
 {
-
-  Console.Clear();
-  Console.WriteLine("===================================================");
-  Console.WriteLine($"Conta Corrente #{numeroIdentificacao} de {titular}");
-  Console.WriteLine("===================================================");
-  Console.WriteLine("1 - Saque");
-  Console.WriteLine("2 - Depósito");
-  Console.WriteLine("3 - Consulta de Saldo");
-  Console.WriteLine("S - Sair");
-  string? opcaoMenu = Console.ReadLine()?.ToUpper();
-
-  if (opcaoMenu == "S")
-    break;
-
-  if (opcaoMenu == "1")
+  static void Main(string[] args)
   {
-    Console.Write("Digite o valor que deseja sacar (R$): ");
-    decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
 
-    if (saldo <= -limiteDebito)
+    ContaCorrente contaUm = new ContaCorrente();
+    contaUm.numeroIdentificacao = 1;
+    contaUm.titular = "Natalia";
+
+    ContaCorrente contaDois = new ContaCorrente();
+    contaDois.numeroIdentificacao = 2;
+    contaDois.titular = "Julia";
+
+    while (true)
     {
-      Console.WriteLine("O valor do limite de débito já foi ultrapassado.");
-      Console.ReadLine();
+
+      Console.Clear();
+      Console.WriteLine("===================================================");
+      Console.WriteLine($"Conta Corrente #{contaUm.numeroIdentificacao} de {contaUm.titular}");
+      Console.WriteLine("===================================================");
+      Console.WriteLine("1 - Saque");
+      Console.WriteLine("2 - Depósito");
+      Console.WriteLine("3 - Consulta de Saldo");
+      Console.WriteLine("S - Sair");
+      string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+      if (opcaoMenu == "S") break;
+
+      if (opcaoMenu == "1") contaUm.Sacar();
+
+      else if (opcaoMenu == "2") contaUm.Depositar();
+
+      else if (opcaoMenu == "3") contaUm.TransferirPara(contaDois);
+
+      else if (opcaoMenu == "4") contaUm.ObterSaldo();
+
     }
-    else
-    {
-      saldo -= valorSaque;
-      Console.WriteLine("O valor foi sacado com sucesso!");
-      Console.ReadLine();
-    }
-
-  }
-
-  else if (opcaoMenu == "2")
-  {
-    Console.Write("Digite o valor que deseja depositar (R$): ");
-    decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
-
-    saldo += valorDeposito;
-
-    Console.WriteLine("O valor foi depositado com sucesso! ");
-    Console.WriteLine($"O valor do saldo da conta é: {saldo}");
-    Console.ReadLine();
-  }
-
-  else if (opcaoMenu == "3")
-  {
-    Console.WriteLine($"O valor do saldo da conta é: {saldo}");
-    Console.ReadLine();
   }
 }
